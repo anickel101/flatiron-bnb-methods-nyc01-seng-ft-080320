87a71102b3981fa307f2d4c3e4a1c3cfd2597ad7
anickel101 <anson.nickel@gmail.com>
2020-08-31 21:14:12 -0400

    working through basic tests

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,16 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :title, :description, :neighborhood_id, :price, :listing_type, presence: true
+
+  before_create 
+
+
+  def average_review_rating
+    total_rating = self.reviews.sum {|review| review.rating}
+    total_reviews = self.reviews.size
+    total_rating.to_f/total_reviews
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,16 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+
+
+  def duration
+    duration = self.checkout - self.checkin
+    duration.to_i
+  end
+
+  def total_price
+    self.duration * self.listing.price
+  end
+
 end
